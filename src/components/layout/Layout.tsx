ae60664b19f861d9ee090111d9cd3761e0bb142d
박유진 <qkrdbw@naver.com>
2024-06-04 21:13:39 +0900

    create Layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../common/Header'
+import Footer from '../common/Footer'
+
+interface LayoutProps {
+    children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import Header from '../components/common/Header';
-
 const Home = () => {
     return (
         <>
-            <Header />
             <div>Home</div>
         </>
     )
