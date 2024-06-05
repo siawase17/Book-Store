a1f8b0e2dd10df51f7a866052de23183027b15d1
박유진 <qkrdbw@naver.com>
2024-06-06 03:11:17 +0900

    create Title component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
+import Button from "../components/common/Button";
+import Title from "../components/common/Title";
+
 const Home = () => {
     return (
         <>
+            <Title size="small">
+                제목 테스트
+            </Title>
+            <Button size='large' scheme="primary">
+                버튼 테스트
+            </Button>
             <div>Home</div>
         </>
     )
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,13 +1,34 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "background" | "secondary" | "third";
+export type ColorKey = "primary" | "background" | "secondary" | "third";
+export type HeadingSize = "large" | "medium" | "small";
+export type  ButtonSize = "large" | "medium" | "small";
+export type ButtonSheme = "primary" | "normal";
 
 interface Theme {
     name: ThemeName;
     color: {
-        // primary: string;
-        // background: string;
         [key in ColorKey]: string;
     };
+    heading: {
+        [key in  HeadingSize]: {
+            fontSize: string;
+        };
+    };
+    button: {
+        [key in ButtonSize]: {
+            fontSize: string;
+            padding: string;
+        }
+    },
+    buttonScheme: {
+        [key in ButtonSheme]: {
+            color: string;
+            backgroundColor: string;
+        }
+    };
+    borderRadius: {
+        default: string;
+    }
 };
 
 export const light: Theme = {
@@ -18,9 +39,48 @@ export const light: Theme = {
         secondary: "blue",
         third: "green",
     },
+    heading: {
+        large: {
+            fontSize: '2rem'
+        },
+        medium: {
+            fontSize: '1.5rem'
+        },
+        small: {
+            fontSize: '1rem'
+        }
+    },
+    button: {
+        large: {
+            fontSize: '1.5rem',
+            padding: '1rem 2rem'
+        },
+        medium: {
+            fontSize: '1rem',
+            padding: '0.5rem 1rem'
+        },
+        small: {
+            fontSize: '0.75rem',
+            padding: '0.25rem 0.5rem'
+        }
+    },
+    buttonScheme: {
+        primary: {
+            color: 'white',
+            backgroundColor: 'midnightblue'
+        },
+        normal: {
+            color: 'black',
+            backgroundColor: 'lightgray'
+        }
+    },
+    borderRadius: {
+        default: "4px",
+    }
 };
 
 export const dark: Theme = {
+    ...light,
     name: 'dark',
     color: {
         primary: "coral",
