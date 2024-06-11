44e912c2b03cca58e69b101b608bf430b6f9ca81
박유진 <qkrdbw@naver.com>
2024-06-11 22:31:08 +0900

    hook: useBooks

--- src/api/books.api.ts
+++ src/api/books.api.ts
@@ -15,6 +15,16 @@ interface FetchBooksResponse {
 }
 
 export const fetchBooks = async (params: FetchBooksParams) => {
-    const response = await httpClient.get<FetchBooksResponse>('/books', {params: params});
-    return response.data;
+    try {
+        const response = await httpClient.get<FetchBooksResponse>('/books', { params: params });
+        return response.data;
+    } catch (error) {
+        return {
+            books: [],
+            pagination: {
+                totalCount: 0,
+                currentPage: 1,
+            }
+        }
+    };
 };
--- src/constants/querystring.ts
+++ src/constants/querystring.ts
@@ -1,4 +1,5 @@
 export const QUERYSTRING = {
     CATEGORY_ID: 'category_id',
     NEWS: 'news',
+    PAGE: 'page',
 };
