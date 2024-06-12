a64242ad6c77c87e5d94b0f31f29b01043ddb371
박유진 <qkrdbw@naver.com>
2024-06-12 22:29:09 +0900

    style: Books component

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -13,19 +13,30 @@ const Books = () => {
     return (
         <>
             <Title size='large'>도서 검색 결과</Title>
-            <BookStyle>
-                <BooksFilter />
-                <BooksViewSwitcher />
+            <BooksStyle>
+                <div className="filter">
+                    <BooksFilter />
+                    <BooksViewSwitcher />
+                </div>
                 {!isEmpty && <BooksList books={books} />}
                 {isEmpty && <BooksEmpty />}
                 {!isEmpty && <Pagination />}
-            </BookStyle>
+            </BooksStyle>
         </>
     )
 };
 
-const BookStyle = styled.div`
-    
+const BooksStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 24px;
+
+    .filter {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 0;
+    }
 `;
 
 export default Books;
