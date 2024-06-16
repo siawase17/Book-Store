686e734fd78d2ebae954fefd714a9376adb9ef68
박유진 <qkrdbw@naver.com>
2024-06-17 03:19:09 +0900

    feat: Empty state, cart summary

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,16 +2,19 @@ import styled from "styled-components";
 import Title from "../components/common/Title"
 import CartItem from "../components/cart/CartItem";
 import { useCart } from "../hooks/useCart";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import Empty from "../components/common/Empty";
+import { FaShoppingCart } from 'react-icons/fa';
+import CartSummary from "../components/cart/CartSummary";
 
 const Cart = () => {
-    const { carts, deleteCartItem } = useCart();
+    const { carts, deleteCartItem, isEmpty } = useCart();
     const [checkedItems, setCheckedItems] = useState<number[]>([]);
     const handleCheckItem = (id: number) => {
         if (checkedItems.includes(id)) {
             setCheckedItems(checkedItems.filter((item) => item !== id));
         } else {
-            setCheckedItems([...checkedItems,id]);
+            setCheckedItems([...checkedItems, id]);
         }
     };
 
@@ -19,31 +22,77 @@ const Cart = () => {
         deleteCartItem(id);
     };
 
+    const totalQuantity = useMemo(() => {
+        return carts.reduce((acc, cart) => {
+            if (checkedItems.includes(cart.id)) {
+                return acc + cart.quantity
+            }
+            return acc;
+        }, 0)
+    }, [carts, checkedItems]);
+
+    const totalPrice = useMemo(() => {
+        return carts.reduce((acc, cart) => {
+            if (checkedItems.includes(cart.id)) {
+                return acc + (cart.price * cart.quantity);
+            }
+            return acc;
+        }, 0)
+    }, [carts, checkedItems]);
+
     return (
         <>
             <Title size="large">장바구니</Title>
             <CartStyle>
-                <div className="content">
-                    {carts.map((item) => (
-                        <CartItem
-                            key={item.id}
-                            cart={item}
-                            checkedItems={checkedItems}
-                            onCheck={handleCheckItem}
-                            onDelete={handleItemDelete}
+                {!isEmpty && (
+                    <>
+                        <div className="content">
+                            {carts.map((item) => (
+                                <CartItem
+                                    key={item.id}
+                                    cart={item}
+                                    checkedItems={checkedItems}
+                                    onCheck={handleCheckItem}
+                                    onDelete={handleItemDelete}
+                                />
+                            ))}
+                        </div>
+                        <div className="summary">
+                        <CartSummary
+                            totalQuantity={totalQuantity}
+                            totalPrice={totalPrice}
                         />
-                    ))}
-                </div>
-                <div className="summary">
-
-                </div>
+                        </div>
+                    </>
+                )}
+                {isEmpty && (
+                    <Empty
+                        icon={<FaShoppingCart />}
+                        title='장바구니가 비었습니다.'
+                        description={<>장바구니를 채워보세요.</>}
+                    />
+                )}
             </CartStyle>
         </>
     )
 };
 
 const CartStyle = styled.div`
-    
+    display: flex;
+    gap: 24px;
+    justify-content: space-between;
+    padding: 24px 0 0 0;
+
+    .content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+
+    .summary {
+        display: flex;
+    }
 `;
 
 export default Cart
