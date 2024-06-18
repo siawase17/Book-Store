1ea8f19cf8284b1d63a26dcc89264fe6c7bc3afa
박유진 <qkrdbw@naver.com>
2024-06-18 23:53:13 +0900

    feat: alias

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const cracoAlias = require("craco-alias");
+
+module.exports = {
+    plugins: [{
+        plugin: cracoAlias,
+        options: {
+            source: "tsconfig",
+            baseUrl: ".",
+            tsConfigPath: "tsconfig.paths.json",
+        },
+    }],
+};
